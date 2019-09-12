b2a40543369a72279e1fe8f50ef66428187a060b
kingc <1126663073@qq.com>
2019-09-12 08:43:59 +0800

    初始化

---
+++ 小程序/pages/home/home.js
@@ -0,0 +1,87 @@
+// pages/home/home.js
+var app = getApp();
+
+Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        // advertising
+        imgUrls: [
+            "../../images/advers/adver-1.jpg",
+            "../../images/advers/adver-2.jpg",
+            "../../images/advers/adver-3.jpg",
+            "../../images/advers/adver-4.jpg"
+        ],
+        indicatorDots: true, //是否显示面板指示点
+        autoplay: true, //是否自动切换
+        interval: 3000, //自动切换时间间隔,3s
+        duration: 1000, //  滑动动画时长1s
+    },
+
+  turn: function (e) {
+    var param = e.currentTarget.dataset.url;
+      wx.navigateTo({
+        url: '../' + param + '/' + param
+      })
+    },
+
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+
+    },
+
+
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
